3901314b76b5f5c127a4292d5e476d8fd844bf2d
SlavaKashpyr <kashpyr2223@gmail.com>
2024-06-02 00:14:19 +0300

    Создание элемента и списка сообщества

--- app/src/main/java/com/topic3/android/reddit/screens/SubredditsScreen.kt
+++ app/src/main/java/com/topic3/android/reddit/screens/SubredditsScreen.kt
@@ -29,6 +29,7 @@ import androidx.compose.ui.unit.sp
 import androidx.compose.ui.zIndex
 import androidx.constraintlayout.compose.ConstraintLayout
 import com.topic3.android.reddit.R
+import com.topic3.android.reddit.components.BackgroundText
 import com.topic3.android.reddit.models.SubredditModel
 
 val subreddits = listOf(
@@ -190,13 +191,45 @@ fun SubredditDescription(modifier: Modifier, @StringRes descriptionStringRes: In
 }
 
 @Composable
-fun Community(text: String, modifier: Modifier = Modifier) {
-    //TODO add your code here
+fun Community(
+    text: String,
+    modifier: Modifier = Modifier,
+    onCommunityClicked: () -> Unit = {}
+) {
+    Row (modifier = modifier
+        .padding(start = 16.dp)
+        .fillMaxWidth()
+        .clickable { onCommunityClicked.invoke() }
+    ) {
+        Image(
+            bitmap = ImageBitmap.imageResource(id = R.drawable.subreddit_placeholder),
+            contentDescription = stringResource(id = R.string.community_icon),
+            modifier = modifier
+                .size(24.dp)
+                .clip(CircleShape)
+        )
+        Text(
+            fontSize = 10.sp,
+            color = MaterialTheme.colors.primaryVariant,
+            text = text,
+            fontWeight = FontWeight.Bold,
+            modifier = modifier
+                .padding(start = 16.dp)
+                .align(Alignment.CenterVertically)
+        )
+    }
 }
 
 @Composable
 fun Communities(modifier: Modifier = Modifier) {
-    //TODO add your code here
+    mainCommunities.forEach {
+        Community(text = stringResource(it))
+    }
+    Spacer(modifier = modifier.height(4.dp))
+    BackgroundText(stringResource(R.string.communities))
+    communities.forEach {
+        Community(text = stringResource(it))
+    }
 }
 
 @Preview
